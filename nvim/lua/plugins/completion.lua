c68ca75dd5735795ac55bdf67673d46bc18ac2c4
Brock Trappitt (Incub8) <brock@incub8.com.au>
2026-01-26 18:34:11 +0800

    Blink

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,48 +1,48 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",      -- The engine
-    "saadparwaiz1/cmp_luasnip", -- The bridge
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
+  'saghen/blink.cmp',
+  version = '*', -- Use a release tag to download pre-built binaries
+  opts = {
+    -- 1. NO NOISE: Kill the ghost text and snippet clutter
+    appearance = {
+      highlight_ns = vim.api.nvim_create_namespace('blink_cmp'),
+    },
+    sources = {
+      -- We remove 'snippets' from the default list to keep the menu clean
+      default = { 'lsp', 'path', 'buffer' },
+    },
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body) -- This is the "magic" bridge line
-        end,
+    -- 2. KEYMAPS: Your "Senior" Manual Logic
+    keymap = {
+      preset = 'none', -- Disable defaults so they don't fight you
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-e>'] = { 'hide' },
+      ['<C-y>'] = { 'select_and_accept' }, -- Explicit YES
+
+      ['<C-p>'] = { 'select_prev', 'fallback' },
+      ['<C-n>'] = { 'select_next', 'fallback' },
+
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+      -- Enter only confirms if you've manually selected an item
+      ['<CR>'] = { 'accept', 'fallback' },
+    },
+
+    -- 3. BEHAVIOR: No pre-selection, no jumping
+    completion = {
+      list = {
+        selection = {
+          preselect = false,   -- Don't highlight the first item automatically
+          auto_insert = false, -- Don't insert text until you confirm
+        }
       },
-      mapping = cmp.mapping.preset.insert({
-        -- Navigating the menu
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        
-        -- Scrolling documentation
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      menu = {
+        auto_show = true,      -- Pop up while typing, but preselect=false keeps it quiet
+      },
+      ghost_text = { enabled = false }, -- No VS Code-style "shadow" text
+    },
 
-        -- Confirming & Closing
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Explicit YES
-        ['<C-e>'] = cmp.mapping.abort(),                    -- Get out of my face
-
-        -- Enter key logic: Only confirm IF an item is manually selected
-        ['<CR>'] = cmp.mapping.confirm({
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = false, 
-        }),
-      }),
-      sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' }, -- Add this to the list
-        { name = 'buffer' },
-        { name = 'path' },
-      })
-    })
-  end,
+    -- Optional: If you hate the signature help (parameter popups), disable it here
+    signature = { enabled = false },
+  },
 }
