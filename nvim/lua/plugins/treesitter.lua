c167e7c053cf340a29917bbb3ddee6a81c357e67
Brock Trappitt (Incub8) <brock@incub8.com.au>
2026-06-10 12:51:31 +0800

    Newer treesitter...?

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,35 @@
+--return {
+--  "nvim-treesitter/nvim-treesitter",
+--  lazy = false,
+--  build = ":TSUpdate",
+--  config = function()
+--    require('nvim-treesitter').install({
+--        -- Daily Drivers
+--        "ruby", "javascript", "typescript", "python", "php", "lua", "html", "css",
+--        -- Configs & Docs
+--        "markdown", "markdown_inline", "yaml", "json", "bash", "regex",
+--        -- Neovim Internals
+--        "vim", "vimdoc", "query"
+--    }):wait(300000) -- wait max. 5 minutes
+--  end,
+--}
+
 return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
-  build = ":TSUpdate",
+  -- Pass the languages directly to the shell command or the build function
+  build = function()
+    require("nvim-treesitter").install({
+      -- Daily Drivers
+      "ruby", "javascript", "typescript", "python", "php", "lua", "html", "css",
+      -- Configs & Docs
+      "markdown", "markdown_inline", "yaml", "json", "bash", "regex",
+      -- Neovim Internals
+      "vim", "vimdoc", "query"
+    }):wait(300000)
+  end,
   config = function()
-    require('nvim-treesitter').install({
-        -- Daily Drivers
-        "ruby", "javascript", "typescript", "python", "php", "lua", "html", "css",
-        -- Configs & Docs
-        "markdown", "markdown_inline", "yaml", "json", "bash", "regex",
-        -- Neovim Internals
-        "vim", "vimdoc", "query"
-    }):wait(300000) -- wait max. 5 minutes
+    -- Only handle runtime initialization here, no install logic
+    require('nvim-treesitter').setup()
   end,
 }
